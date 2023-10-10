6c1f96ec741a898cc48ea1d52fb9d15e0ac04a04
kbiq <khalid.biq@cegedim-activ.com>
2023-10-10 06:51:04 +0100

    dd

---
+++ src/Exercice.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Exercice {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+
+
+"ex"
+}
